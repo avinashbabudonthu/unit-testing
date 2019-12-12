be6e79d51f7a0b466efc64125fa6675810a12370
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2019-12-12 21:37:38 +0530

    cucumber hello world

---
+++ cucumber/hello-world/src/test/java/com/calculate/CucumberTest.java
@@ -0,0 +1,13 @@
+package com.calculate;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, snippets = SnippetType.CAMELCASE, features = "classpath:StepDefinition")
+public class CucumberTest {
+
+}

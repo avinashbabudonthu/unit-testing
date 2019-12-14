637e2c8c9bfa1cb2de3816d1764858b3395e58b3
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2019-12-14 22:42:25 +0530

    cucumber hello world

--- cucumber/cucumber-level-2/README.md
+++ cucumber/cucumber-level-2/README.md
@@ -1,6 +1,17 @@
 # Cucumber Level 2 Examples
 * Refer [Hello World Examples](https://github.com/avinashbabudonthu/unit-testing/tree/master/cucumber/hello-world#examples) before referring examples in the repository
 
+## Examples
+* [Calculator Sum](#calculator-sum)
+* [Scenario Outline](#scenario-outline)
+* [Mockito](#mockito)
+
+### Calculator Sum
+
+### Scenario Outline
+
+### Mockito
+
 ## Pending POCs
 * Backgroud with list
 * Backgroud with map
--- cucumber/cucumber-level-2/src/test/java/com/cucumber/CucumberTestRunner.java
+++ cucumber/cucumber-level-2/src/test/java/com/cucumber/CucumberTestRunner.java
@@ -1,6 +1,9 @@
 package com.cucumber;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
@@ -8,6 +11,10 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, snippets = SnippetType.CAMELCASE, glue = {
-		"com.calculator.step.definitions" }, features = { "classpath:features" })
+		"com.cucumber.step.definitions" }, features = { "classpath:features/calculator.sum",
+				"classpath:features/scenario.outline", "classpath:features/mockito" })
 public class CucumberTestRunner {
+
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 }
